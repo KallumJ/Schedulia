d3325d6a536f4f5ceaba01ef6b98b26655445afb
Kallum Jones <57623845+KallumJ@users.noreply.github.com>
2022-09-06 17:27:48 +0100

    Started media event component

---
+++ components/schedule/MediaEvent.tsx
@@ -0,0 +1,7 @@
+import styles from "../../styles/components/schedule/MediaEvent.module.scss";
+
+export default function MediaEvent() {
+    return (
+        <div className={styles.event}>MediaEvent</div>
+    )
+}
