13fef50276ad860843332fe07e136457126a066f
Kallum Jones <kallummj071001@gmail.com>
2022-09-26 15:24:35 +0100

    Add day names to the calendar (fixes #3)

--- components/schedule/Month.tsx
+++ components/schedule/Month.tsx
@@ -10,16 +10,27 @@ interface MonthProps {
 }
 
 export default function Month({ month, events }: MonthProps) {
-    const days = [];
+    const daysElements = [];
+    const firstSevenDays = []
     for (let i = 1; i <= DateUtils.getDaysInMonth(month); i++) {
         const date = new Date(month.getFullYear(), month.getMonth(), i);
+        if (i <= 7) {
+            firstSevenDays.push(date);
+        }
+
         const daysEvents = events.filter(event => event.releaseDate && DateUtils.areDatesEqual(new Date(event.releaseDate), date))
-        days.push(<Day key={date.getDate()} date={date} events={daysEvents} />);
+        daysElements.push(<Day key={date.getDate()} date={date} events={daysEvents} />);
     }
 
     return (
         <div className={styles.grid}>
-            {days}
+            <div className={styles.gridHeader}>
+                {firstSevenDays.map(day => <label>{day.toLocaleDateString("en-GB", { weekday: "long" })}</label>)}
+            </div>
+            <div className={styles.gridContent}>
+                {daysElements}
+            </div>
+
         </div>
     )
 }
